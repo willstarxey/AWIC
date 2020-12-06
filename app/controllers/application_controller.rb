53e4a0e4d39cf3e274bdaa7f037e875343b9a180
Guillermo Rios <willstarxey@gmail.com>
2020-12-06 00:28:02 -0600

    fixed app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    #before_action :authenticate_user!
     
     add_flash_types :success, :danger, :info, :warning
 
