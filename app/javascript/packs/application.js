c281002df82043db3ff35e485d79ba84f35fd5ab
Guillermo Rios <willstarxey@gmail.com>
2020-12-06 12:47:46 -0600

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("data-confirm-modal")
+require("jquery")
 
 global.toastr = require("toastr")
 import "../stylesheets/application"
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
